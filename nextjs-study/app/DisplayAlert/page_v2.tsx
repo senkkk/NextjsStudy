702d97108e114d988933f38be88a52f08c64e6b7
senkkk <83765743+senkkk@users.noreply.github.com>
2025-05-01 12:17:42 +0900

    #1 Alert表示機能の改善：メッセージ表示中に新しいメッセージを処理するロジックを追加し、複数メッセージの表示を検討

--- nextjs-study/app/DisplayAlert/page_v2.tsx
+++ nextjs-study/app/DisplayAlert/page_v2.tsx
@@ -16,36 +16,19 @@ export default function AlertExample() {
   });
 
   const handleShowAlert = (severity: "success" | "error", message: string) => {
-    if (alertState.open) {
-      setAlertState((prev) => ({ ...prev, open: false }));
-
-      //openをfalseに変更してから、実際にMaterial-UIのSnackbarが閉じるまでには約225msかかると言われているため、
-      //300ms待ってから、openをtrueに変更する
-      setTimeout(() => {
-        setAlertState({ open: true, severity, message });
-      }, 300);
-    } else {
-      setAlertState({ open: true, severity, message });
-    }
+    setAlertState({ open: true, severity, message });
   };
 
   const handleCloseAlert = () => {
     setAlertState((prev) => ({ ...prev, open: false }));
   };
 
+  //このコードである程度整理することはできたが、メッセージは一つしか表示できない。
+  //そのため、成功メッセージが表示されている途中でエラーメッセージを表示した場合、メッセージ内容は更新されるが、
+  //AutoHideDurationを再設定シているわけではないため、すぐに消えてしまう。
   return (
     <>
       <h1>Material UIでのメッセージ表示コンポーネント</h1>
-      <h2>やったこと</h2>
-      <li>
-        ボタンを押下することで、汎用的なメッセージ表示関数を呼び出し、成功・エラー、メッセージ文言を切り替えて表示する
-      </li>
-      <li>
-        メッセージ表示中に、別のメッセージ処理が差し込まれた場合には、一旦メッセージを閉じたうえで再表示する処理を実装
-      </li>
-      <li>
-        同時に複数件のメッセージを表示する仕様も検討したが、実装が複雑になる可能性があったため一旦PENDING
-      </li>
       <Stack spacing={2} direction="row" sx={{ marginTop: "20px" }}>
         <Button
           variant="contained"
